e6254864fd512955a8f60605c1cd71216be95952
typ0520 <php12345@163.com>
2021-02-03 10:48:19 +0800

    bsc prod

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,13 +1,13 @@
 import { chainConfig, ensumeChainId } from '../components/wallet/Config'
 
 export const config = {
-    3 : {
-        quoteFactoryContractAddress: '0x6F5a802977D149714Af838259b85F6a0aE32eb09',
-        poolFactoryContractAddress: '0xe62514AfAe531eaBEb00fF9a9Df651D1e650EC91',
-        poolProxyContractAddress: '0xC0bee094F57B82BE8927B9bA962dA0bd3f0D38Bf',
-        bonusRecordContractAddress: '0x2f43E1d5D90c116E100aa4158B2B5214343b9a9F',
-        baseUrl: 'http://47.90.62.21:9003',
-        quoteWS: 'ws://test.trade.idefiex.com:9002',
+    56 : {
+        quoteFactoryContractAddress: '0xB861098F092244A0a0Aaf8b1d7CcaD703Dbe45D4',
+        poolFactoryContractAddress: '0xcF7a5DCd94c8d8f1Dfc353D1FF3016DDb9335BC7',
+        poolProxyContractAddress: '0xb687494958E24745F8F4198ffEcD035837fc2031',
+        bonusRecordContractAddress: '0xa30cc0b794Fc8137A8a9191E97A62C4f8120469d',
+        baseUrl: 'https://mvp.teemo.finance',
+        quoteWS: 'wss://mvp.teemo.finance/ws',
     },
     97: {
         quoteFactoryContractAddress: '0xB861098F092244A0a0Aaf8b1d7CcaD703Dbe45D4',
